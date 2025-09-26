df50d0f38c10f52d494a2a8f2868a32b3bd36575
Kyaw Thura <tkyaw2404@gmail.com>
2025-09-26 16:19:04 +0630

    update default route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { GetAllCustomer } from './components/get-all-customer/get-all-customer';
 import { UpdateCustomer } from './components/update-customer/update-customer';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/customers', pathMatch: 'full' },
   { path: 'customer', component: PostCustomer },
   { path: 'customers', component: GetAllCustomer },
   { path: 'edit-customer/:id', component: UpdateCustomer },
